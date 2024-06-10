20c2516866ea13ddd43d28dbdca579c40f134768
Rafał Szymura <raszymura@gmail.com>
2024-06-10 16:31:43 +0200

    demo: Discover Bluetooth devices

---
+++ demo/discover_BLE_devices.py
@@ -0,0 +1,11 @@
+# Discover Bluetooth devices
+
+import asyncio
+from bleak import BleakScanner
+
+async def main():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d)
+
+asyncio.run(main())
